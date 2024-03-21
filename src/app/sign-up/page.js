fb69b47a36e46bf5dd230d9d14c9597a4a87814d
Hugh Culling <hughculling@icloud.com>
2024-03-21 10:43:49 +0000

    Added 'sign-up' page.

---
+++ src/app/sign-up/page.js
@@ -0,0 +1,41 @@
+export default function Page() {
+  return (
+    <>
+      <h2>Sign Up</h2>
+      <table>
+        <form method="post">
+          <tr>
+            <td>
+              <label htmlFor="name">Name:</label>
+            </td>
+            <td>
+              <input type="text" id="name" name="name" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="email">Email:</label>
+            </td>
+            <td>
+              <input type="email" id="email" name="email" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="password">Password:</label>
+            </td>
+            <td>
+              <input type="password" id="password" name="password" />
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+              <input type="submit" value="Submit" />
+            </td>
+          </tr>
+        </form>
+      </table>
+    </>
+  );
+}

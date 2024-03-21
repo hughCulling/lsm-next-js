3c92a74e2028f51270dd17932acac5d272221e7e
Hugh Culling <hughculling@icloud.com>
2024-03-21 10:45:34 +0000

    Added 'tbody'.

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -3,38 +3,40 @@ export default function Page() {
     <>
       <h2>Sign Up</h2>
       <table>
-        <form method="post">
-          <tr>
-            <td>
-              <label htmlFor="name">Name:</label>
-            </td>
-            <td>
-              <input type="text" id="name" name="name" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="email">Email:</label>
-            </td>
-            <td>
-              <input type="email" id="email" name="email" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="password">Password:</label>
-            </td>
-            <td>
-              <input type="password" id="password" name="password" />
-            </td>
-          </tr>
-          <tr>
-            <td></td>
-            <td>
-              <input type="submit" value="Submit" />
-            </td>
-          </tr>
-        </form>
+        <tbody>
+          <form method="post">
+            <tr>
+              <td>
+                <label htmlFor="name">Name:</label>
+              </td>
+              <td>
+                <input type="text" id="name" name="name" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="email">Email:</label>
+              </td>
+              <td>
+                <input type="email" id="email" name="email" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="password">Password:</label>
+              </td>
+              <td>
+                <input type="password" id="password" name="password" />
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>
+                <input type="submit" value="Submit" />
+              </td>
+            </tr>
+          </form>
+        </tbody>
       </table>
     </>
   );
